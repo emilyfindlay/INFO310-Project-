3f0b3baf92a364b1743ace7b8ac925ea1d363005
wakev951 <wakev951@student.otago.ac.nz>
2025-03-24 16:30:40 +1300

    gui classess for internal testing created

---
+++ src/main/java/gui/BusinessInfoEditor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui;
+
+/**
+ *
+ * @author kevin
+ * 
+ * Business info -> user's information. This is used to populate fields for the from section of an invoice
+ * 
+ */
+public class BusinessInfoEditor {
+    
+}
