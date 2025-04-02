4191b0e9912d014bac0c2c6c38355af5c62f919c
emilyfindlay <emilyfindlay@localhost>
2025-04-03 11:19:31 +1300

    finalised the address domain class, matches ERD now

--- src/main/java/domain/Address.java
+++ src/main/java/domain/Address.java
@@ -9,24 +9,29 @@ package domain;
  * @author kevin
  */
 public class Address {
-    private Integer addressId;
-    private String streetAddress;
-    private String suburb;
-    private String city;
-    private Integer postCode;
 
-    
-    public Address(){
-        
+    private Integer addressId;
+    private String streetAddress1;
+    private String streetAddress2;
+    private String city;
+    private String region;
+    private Integer postCode;
+    private String country;
+
+    public Address() {
+
     }
-    
-    public Address(String streetAddress, String suburb, String city, Integer postCode) {
-        this.streetAddress = streetAddress;
-        this.suburb = suburb;
+
+    public Address(Integer addressId, String streetAddress1, String streetAddress2, String city, String region, Integer postCode, String country) {
+        this.addressId = addressId;
+        this.streetAddress1 = streetAddress1;
+        this.streetAddress2 = streetAddress2;
         this.city = city;
+        this.region = region;
         this.postCode = postCode;
+        this.country = country;
     }
-    
+
     public Integer getAddressId() {
         return addressId;
     }
@@ -35,20 +40,20 @@ public class Address {
         this.addressId = addressId;
     }
 
-    public String getStreetAddress() {
-        return streetAddress;
+    public String getStreetAddress1() {
+        return streetAddress1;
     }
 
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
+    public void setStreetAddress1(String streetAddress1) {
+        this.streetAddress1 = streetAddress1;
     }
 
-    public String getSuburb() {
-        return suburb;
+    public String getStreetAddress2() {
+        return streetAddress2;
     }
 
-    public void setSuburb(String suburb) {
-        this.suburb = suburb;
+    public void getStreetAddress2(String streetAddress2) {
+        this.streetAddress2 = streetAddress2;
     }
 
     public String getCity() {
@@ -58,6 +63,14 @@ public class Address {
     public void setCity(String city) {
         this.city = city;
     }
+    
+    public String getRegion(){
+        return region;
+    }
+    
+    public void setRegion(String region) {
+        this.region = region;
+    }
 
     public Integer getPostCode() {
         return postCode;
@@ -66,12 +79,26 @@ public class Address {
     public void setPostCode(Integer postCode) {
         this.postCode = postCode;
     }
+    
+    public String country(){
+        return country;
+    }
+    
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     @Override
     public String toString() {
-        return super.toString();    //TODO
+        return "Address{" + 
+                "addressId=" + addressId + 
+                ", streetAddress1='" + streetAddress1 + '\'' +
+                ", streetAddress2='" + streetAddress2 + '\'' + 
+                ", city='" + city + '\'' +
+                ", region='" + region + '\'' + 
+                ", postCode='" + postCode + '\'' + 
+                ", country='" + country + '\'' + 
+                '}';
     }
-    
-    
-    
+
 }
