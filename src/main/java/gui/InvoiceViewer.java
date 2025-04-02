624aa31aa6474db81894611f6241adbff5145236
wakev951 <wakev951@student.otago.ac.nz>
2025-04-02 15:27:18 +1300

    added rough gui for product, invoice, and client viewers. Added code to make buttons work, waitin gon DAO classes

--- src/main/java/gui/InvoiceViewer.java
+++ src/main/java/gui/InvoiceViewer.java
@@ -5,6 +5,7 @@
 package gui;
 
 import helpers.SimpleListModel;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -12,9 +13,9 @@ import helpers.SimpleListModel;
  */
 public class InvoiceViewer extends javax.swing.JDialog {
 
-    //private final ProductDAO dao;
+    //private final InvoiceDAO dao;
 
-	private final SimpleListModel productsModel = new SimpleListModel();
+	private final SimpleListModel invoicesModel = new SimpleListModel();
 
 	public InvoiceViewer(java.awt.Frame parent, boolean modal){         //, ProductDAO dao) {
 		super(parent, modal);
@@ -24,9 +25,9 @@ public class InvoiceViewer extends javax.swing.JDialog {
 		initComponents();
 //
 //		// load products into JList
-//		Collection<Product> products = dao.getProducts();
-//		productsModel.updateItems(products);
-//		lstProducts.setModel(productsModel);
+//		Collection<Invoice> invoices = dao.getInvoices();
+//		invoicesModel.updateItems(invoices);
+//		lstInvoice.setModel(invoicesModel);
 //
 //		// load categories into combo
 //		SimpleListModel categoriesModel = new SimpleListModel();
@@ -46,23 +47,147 @@ public class InvoiceViewer extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        txtSearchId = new javax.swing.JTextField();
+        btnSearch = new javax.swing.JButton();
+        lblCategory = new javax.swing.JLabel();
+        cmbCategories = new javax.swing.JComboBox<>();
+        btnClose = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        lstInvoice = new javax.swing.JList<>();
+        btnDelete = new javax.swing.JButton();
+        lblId = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        btnSearch.setText("Search");
+        btnSearch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSearchActionPerformed(evt);
+            }
+        });
+
+        lblCategory.setText("Category:");
+
+        cmbCategories.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmbCategoriesActionPerformed(evt);
+            }
+        });
+
+        btnClose.setText("Close");
+        btnClose.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCloseActionPerformed(evt);
+            }
+        });
+
+        jScrollPane1.setViewportView(lstInvoice);
+
+        btnDelete.setText("Delete");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
+
+        lblId.setText("ID:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(12, 12, 12)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnDelete, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(157, 157, 157)
+                        .addComponent(btnClose, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(36, 36, 36)
+                                .addComponent(lblId))
+                            .addComponent(lblCategory, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txtSearchId)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnSearch))
+                            .addComponent(cmbCategories, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE))
+                .addGap(12, 12, 12))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblId)
+                    .addComponent(txtSearchId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSearch))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblCategory)
+                    .addComponent(cmbCategories, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 184, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnClose)
+                    .addComponent(btnDelete))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSearchActionPerformed
+        String id = txtSearchId.getText();
+        Invoice invoice = dao.searchById(id);
+        invoicesModel.updateItems(invoice);
+    }//GEN-LAST:event_btnSearchActionPerformed
+
+    private void cmbCategoriesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbCategoriesActionPerformed
+        String category = (String) cmbCategories.getSelectedItem();
+        Collection<Invoice> products = dao.filterByCategory(category);
+        invoicesModel.updateItems(invoices);
+    }//GEN-LAST:event_cmbCategoriesActionPerformed
+
+    private void btnCloseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCloseActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_btnCloseActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        if (!lstInvoice.isSelectionEmpty()) {
+            Invoice selected = lstInvoice.getSelectedValue();
+
+            int result = JOptionPane.showConfirmDialog(this, "Delete invoice " + selected.getName() + "?", "Confirm Delete", JOptionPane.YES_NO_OPTION);
+
+            // did the user click the yes button?
+            if (result == JOptionPane.YES_OPTION) {
+                dao.removeInvoice(selected);
+
+                // update JList
+                productsModel.updateItems(dao.getInvoices());
+
+                // selected item was deleted, so clear selection
+                lstInvoice.clearSelection();
+            }
+        }
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnClose;
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnSearch;
+    private javax.swing.JComboBox<String> cmbCategories;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblCategory;
+    private javax.swing.JLabel lblId;
+    private javax.swing.JList<Invoice> lstInvoice;
+    private javax.swing.JTextField txtSearchId;
     // End of variables declaration//GEN-END:variables
 }
