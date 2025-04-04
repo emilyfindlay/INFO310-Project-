3d68516596858b90a895157d6fa40d60fdea004e
emilyfindlay <emilyfindlay@localhost>
2025-04-04 18:58:19 +1300

    Added oVal to the Business class

--- src/main/java/domain/Business.java
+++ src/main/java/domain/Business.java
@@ -6,37 +6,61 @@ package domain;
 
 import jakarta.persistence.*;
 import java.util.Objects;
+import net.sf.oval.constraint.Length;
+import net.sf.oval.constraint.NotBlank;
+import net.sf.oval.constraint.NotNull;
+import net.sf.oval.constraint.Range;
 
 @Entity
 @Table(name = "business")
 public class Business {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "business_id")
+    @NotNull(message = "Business ID must be provided")
+    @Length(min = 2, message = "Business ID must have 2 or more characters")
     private Integer businessId;
 
     @ManyToOne
     @JoinColumn(name = "address_id", nullable = false)
+    @NotNull(message = "Address ID must be provided")
+    @Length(min = 2, message = "ID must contain at least two characters.")
     private Address address;
 
     @Column(name = "business_name", nullable = false, length = 50)
+    @NotNull(message = "business name must be provided")
+    @NotBlank(message = "business name must be provided") 
+    @Range(min = 2, max = 50, message = "business name must be more than 2 characters but less than 50 characters")
     private String businessName;
 
+    @NotNull(message = "bank account name must be provided")
+    @NotBlank(message = "bank account name must be provided")
+    @Range(min = 2, max = 50, message = "bank account name must be greater than 2 and less than 50")
     @Column(name = "bank_account_name", nullable = false, length = 50)
     private String bankAccountName;
 
+    @Length(min = 2, message = "business description must be more than 2 characters")
     @Column(name = "business_description")
     private String businessDescription;
 
+    @Range(min = 2, max = 15, message = "GST number has to be greater than 2 and less than 15")
     @Column(name = "gst_number", length = 15)
     private String gstNumber;
 
+    @NotNull(message = "email must be provided")
+    @NotBlank(message = "email must be provided")
+    @Range(min = 2, max = 50, message = "email must be larger than 2 characters but less than 50 characters")
     @Column(name = "email", nullable = false, length = 50)
     private String email;
 
+    @NotNull(message = "phone must be provided") 
+    @NotBlank(message = "phone must be provided") 
+    @Range(min = 2, max = 10, message = "phone must be more than 2 characters and less than 10 characters")
     @Column(name = "phone", nullable = false, length = 10)
     private String phone;
 
+    @Length(min = 2, message = "website link must be greater than 2 character")
     @Column(name = "website_link", length = 255)
     private String websiteLink;
 
@@ -44,12 +68,13 @@ public class Business {
     @Column(name = "logo")
     private byte[] logo;
 
-    public Business() {}
+    public Business() {
+    }
 
     public Business(Integer businessId, Address address, String businessName,
-                   String bankAccountName, String businessDescription,
-                   String gstNumber, String email, String phone,
-                   String websiteLink, byte[] logo) {
+            String bankAccountName, String businessDescription,
+            String gstNumber, String email, String phone,
+            String websiteLink, byte[] logo) {
         this.businessId = businessId;
         this.address = address;
         this.businessName = businessName;
@@ -144,12 +169,16 @@ public class Business {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Business business = (Business) o;
         return Objects.equals(businessId, business.businessId);
     }
-    
+
     @Override
     public int hashCode() {
         return Objects.hash(businessId);
@@ -157,10 +186,10 @@ public class Business {
 
     @Override
     public String toString() {
-        return "Business{" +
-               "businessId=" + businessId +
-               ", businessName='" + businessName + '\'' +
-               ", email='" + email + '\'' +
-               '}';
+        return "Business{"
+                + "businessId=" + businessId
+                + ", businessName='" + businessName + '\''
+                + ", email='" + email + '\''
+                + '}';
     }
 }
